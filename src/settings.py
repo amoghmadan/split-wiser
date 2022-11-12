94573bf16bd520ea69ec533ed18738f7552a8514
Amogh Madan <amoghmadaan@gmail.com>
2022-11-12 14:25:55 +0530

    Incorporate database and migration settings

--- src/settings.py
+++ src/settings.py
@@ -2,12 +2,16 @@ from pathlib import Path
 import configparser
 import os
 
-ENVIRONMENT = os.environ.setdefault("SPLIT_WISER", "development")
+ENV = os.environ.setdefault("SPLIT_WISER", "development")
 
 BASE_DIR = Path(__file__).resolve().parent
 RESOURCES_DIR = BASE_DIR.parent / "resources"
 
 CONFIG = configparser.ConfigParser()
-CONFIG.read(RESOURCES_DIR / f"{ENVIRONMENT}.ini")
+CONFIG.read(RESOURCES_DIR / f"{ENV}.ini")
 
 DEBUG = CONFIG.getboolean("DEFAULT", "DEBUG")
+SQLALCHEMY_DATABASE_URI = CONFIG.get("DEFAULT", "SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_TRACK_MODIFICATIONS = CONFIG.getboolean(
+    "DEFAULT", "SQLALCHEMY_TRACK_MODIFICATIONS"
+)
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,14 +1,29 @@
 from flask import Flask
 
+from routes import routes
+from utilities import db, ma, migrate
 import settings
 
 
 def get_application():
+    """Get application"""
     app = Flask(__name__)
+    app.config.update(
+        DEBUG=settings.DEBUG,
+        ENV=settings.ENV,
+        SQLALCHEMY_DATABASE_URI=settings.SQLALCHEMY_DATABASE_URI,
+        SQLALCHEMY_TRACK_MODIFICATIONS=settings.SQLALCHEMY_TRACK_MODIFICATIONS,
+    )
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+    for prefix, route in routes:
+        app.register_blueprint(route, url_prefix=prefix)
     return app
 
 
 application = get_application()
 
 if __name__ == "__main__":
-    application.run(debug=settings.DEBUG)
+    """Run application (development)"""
+    application.run()
