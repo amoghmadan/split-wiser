87b4ad4d21f37659d376e21470caf163be431617
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2022-11-29 13:27:56 +0530

    Added TokenViewSet

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,6 +1,7 @@
 from views.friends import FriendViewSet
 from views.users import UserViewSet
 from views.groups import GroupViewSet
+from views.token import TokenViewSet
 
 __all__ = [
     "FriendViewSet",
