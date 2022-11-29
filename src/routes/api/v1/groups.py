e6ae82eee133b136dff2ba37a014ba50347ad159
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2022-11-29 20:57:52 +0530

    Changed group --> groups

--- src/routes/api/v1/groups.py
+++ src/routes/api/v1/groups.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 from views import GroupViewSet
 
-group = Blueprint("user", __name__)
-group.add_url_rule(
+groups = Blueprint("groups", __name__)
+groups.add_url_rule(
     "/", view_func=GroupViewSet.as_view("group-list"), methods=["GET", "POST"]
 )
-group.add_url_rule(
+groups.add_url_rule(
     "/<id>",
     view_func=GroupViewSet.as_view("group-detail"),
     methods=["GET", "PUT", "PATCH", "DELETE"]
