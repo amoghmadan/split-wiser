e708171a4b9e4a99c44126e6364b3e0578be2a20
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2022-11-29 20:57:33 +0530

    Changed friend --> friends

--- src/routes/api/v1/friends.py
+++ src/routes/api/v1/friends.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 from views import FriendViewSet
 
-friend = Blueprint("friend", __name__)
-friend.add_url_rule(
+friends = Blueprint("friends", __name__)
+friends.add_url_rule(
     "/", view_func=FriendViewSet.as_view("friend-list"), methods=["GET", "POST"]
 )
-friend.add_url_rule(
+friends.add_url_rule(
     "/<id>",
     view_func=FriendViewSet.as_view("friend-detail"),
     methods=["GET", "PUT", "PATCH", "DELETE"]
