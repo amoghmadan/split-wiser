64da4b80eb7f66282db5a355e346fdbbe37c28ce
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2022-11-29 20:22:44 +0530

    Created Token ViewSet

--- src/views/tokens.py
+++ src/views/tokens.py
@@ -1,19 +1,25 @@
 import binascii
 import os
+
 from flask import views, request
+from sqlalchemy.exc import IntegrityError
 
 from models import Token, User
+from schemas import TokenSchema
 from utilities import db
 
 
 class TokenViewSet(views.MethodView):
+    model = Token
+    schema_class = TokenSchema
 
-    def generate_key(self):
+    @classmethod
+    def generate_key(cls):
         """It will generate the token"""
         return binascii.hexlify(os.urandom(20)).decode()
 
     def get(self, *args, **kwargs):
-        # breakpoint()
+
         auth = request.authorization
         if not auth or not auth.username or not auth.password:
             return {"message": "could not verify"}
@@ -21,10 +27,15 @@ class TokenViewSet(views.MethodView):
         instance = User.query.filter_by(first_name=auth.username).first()
 
         if instance.password == auth.password:
-            token = self.generate_key()
-            data = Token(user_id=instance.id, key=token)
-            db.session.add(data)
-            db.session.commit()
-            return {"token": token}
+            try:
+                token = Token(user_id=instance.id)
+                db.session.add(token)
+                db.session.commit()
+                db.session.flush()
+                db.session.refresh(token)
+            except IntegrityError:
+                db.session.rollback()
+                token = Token.query.filter_by(user_id=instance.id).first()
+            return {"token": token.key}
 
         return {"message": "Some Error"}
