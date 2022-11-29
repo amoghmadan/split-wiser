869c3b80c924d78766595b555bfed4375333a15d
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2022-11-29 20:58:56 +0530

    Changed token -> tokens

--- src/routes/api/v1/tokens.py
+++ src/routes/api/v1/tokens.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from views import TokenViewSet
 
-token = Blueprint("token", __name__)
-token.add_url_rule(
+tokens = Blueprint("tokens", __name__)
+tokens.add_url_rule(
     "/", view_func=TokenViewSet.as_view("token-list"), methods=["GET", "POST"]
 )
